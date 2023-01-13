9ed82e6c82969863b43b75080779105d9ebc23db
HaroSierra <118926575+HaroSierra@users.noreply.github.com>
2023-01-13 14:56:22 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Mi Práctica con CodeSandBox</h1>
+<h2>Jocelyne Haro Sierra</h2>
+<div>
+  Universidad Tecnológica de Durango 
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">Mi Github</a>.
+</div>
+`;
